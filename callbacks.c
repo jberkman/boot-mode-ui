40ceeaf65da52efd37579370518d6c3b13bc5236
jacob berkman <jacob@87k.net>
2007-12-13 19:21:38 +0000

    - initial import
    git-svn-id: http://loolixbodes.googlecode.com/svn/trunk@19 aa23fe99-163d-0410-8ed0-b94f67433ec0

---
+++ callbacks.c
@@ -0,0 +1,42 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_restart_button_clicked              (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_normal_button_clicked               (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_update_button_clicked               (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_close_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ callbacks.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+
+void
+on_restart_button_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_normal_button_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_update_button_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_close_button_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
